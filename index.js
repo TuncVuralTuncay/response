3937fb61674090bf6a2d84b722245a01ab74e932
Tuncvrjs <90102420+Tuncvrjs@users.noreply.github.com>
2022-05-23 21:57:03 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const read = require("readline").createInterface({
+input : process.stdin,
+output : process.stdout
+})
+read.question("Lütfen url giriniz : ").then(a=>
+.setTimeout(() =>{
+const fetch = require("node-fetch") 
+fetch(a) 
+console.log("Request has been send successfully") 
+}, 3000)
+
+
+
+
+
