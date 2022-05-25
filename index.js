8eb55182de1188a7b4650c80b57c1ac2c5032c25
Tuncvrjs <90102420+Tuncvrjs@users.noreply.github.com>
2022-05-25 19:24:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ var dat = new Date()
 h = dat.getHours()
 m = dat.getMinutes()
 s = dat.getSeconds()
-if(h > 10) h = "0" + h
-if(m > 10) m = "0" + m
-if(s > 10) s = "0" + s
+if(h < 10) h = "0" + h
+if(m < 10) m = "0" + m
+if(s < 10) s = "0" + s
 console.log(`
 [ ${chalk.green("OK")} ]
 ${chalk.blue("Request has been send successfully")} 
-${h}:${m}:${s}
-`) 
+${h}:${m}:${s}`) 
 }, 3000)
 }) 
 
